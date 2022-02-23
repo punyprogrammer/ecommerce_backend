b788258f45b999cbf9d9e4ea5c175af93a22f441
Amardeep Ganguly <you@example.com>
2022-02-24 02:23:48 +0530

    'fixed-stripe-key-issue'

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 // const stripe = require("stripe")(process.env.STRIPE_KEY);
-const KEY = process.env.STRIPE_KEY;
+const KEY ='sk_test_51EZ5rqGv1KwaSbc9NNHEdbPNvF6tSHTuc2u1YL1EPkh500jdTsV0W3uNGaIttE6g1slS2JSbxg2ISbCV7lokcalI00lR57ZIEN'
 const stripe = require("stripe")(KEY);
 
 router.post("/payment", (req, res) => {
